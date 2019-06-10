351f3fdc8990675c61b5f84bbc195571dbf41832
tuanth89 <trantuan101989@gmail.com>
2019-06-10 23:19:38 +0700

    Fixed: - remove 1 customer lead to remove all contract

--- src/events/listeners/customerChangeListener.js
+++ src/events/listeners/customerChangeListener.js
@@ -15,45 +15,58 @@ const ObjectId = require('mongoose').Types.ObjectId;
  * @param {ObjectId} visitorId
  */
 function removeAllByCustomerId(customerId, visitorId) {
-    Contract.remove({
-        customerId: customerId
-    }, function (error) {
-        if (error) {
-            log.error(error);
-        }
-    });
+    Contract
+        .find({customerId: ObjectId(customerId)})
+        .select('_id')
+        .exec(function (err, contracts) {
+            let contractIds = _.map(contracts, function (element, idx) {
+                return ObjectId(element._id);
+            });
 
-    ContractLog.remove({
-        customerId: customerId
-    }, function (error) {
-        if (error) {
-            log.error(error);
-        }
-    });
+            Contract.remove({
+                customerId: customerId
+            }, function (error) {
+                if (error) {
+                    log.error(error);
+                }
+            });
 
-    HdLuuThong.remove({
-        creator: visitorId
-    }, function (error) {
-        if (error) {
-            log.error(error);
-        }
-    });
+            ContractLog.remove({
+                customerId: customerId
+            }, function (error) {
+                if (error) {
+                    log.error(error);
+                }
+            });
 
-    HdLuuThongOther.remove({
-        creator: visitorId
-    }, function (error) {
-        if (error) {
-            log.error(error);
-        }
-    });
+            HdLuuThong.deleteMany({
+                // creator: visitorId,
+                contractId: {$in: contractIds}
+            }, function (error) {
+                if (error) {
+                    log.error(error);
+                }
+            });
 
-    ReportDaily.remove({
-        creator: visitorId
-    }, function (error) {
-        if (error) {
-            log.error(error);
-        }
-    });
+            HdLuuThongOther.deleteMany({
+                // creator: visitorId
+                contractId: {$in: contractIds}
+            }, function (error) {
+                if (error) {
+                    log.error(error);
+                }
+            });
+
+            // ReportDaily.remove({
+            //     creator: visitorId
+            // }, function (error) {
+            //     if (error) {
+            //         log.error(error);
+            //     }
+            // });
+
+        });
+
 }
 
 /**
